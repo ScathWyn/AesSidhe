0a056d010da54dac667fc7b4175caef912621eb7
ScathWyn <19636890+ScathWyn@users.noreply.github.com>
2018-05-27 11:31:52 +0200

    Connection with form added. SecurityController added to manage sign in, sign out, and register

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,17 +6,44 @@ use App\Entity\User;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
-class ConnectionController extends Controller
+class SecurityController extends Controller
 {
     /**
-     * @Route("/connection", name="connection", host="aesSidhe.fr")
+     * @Route("/signIn", name="sign_in", host="aesSidhe.fr")
      */
-    public function index(Request $request)
+    public function signIn(Request $request, AuthenticationUtils $authenticationUtils)
+    {
+		
+		// get the login error if there is one
+		$error = $authenticationUtils->getLastAuthenticationError();
+
+		// last username entered by the user
+		$lastUsername = $authenticationUtils->getLastUsername();
+		
+        return $this->render('security/sign_in/index.html.twig', [
+            'last_username' => $lastUsername,
+			'error'         => $error,
+        ]);
+    }
+	
+	/**
+     * @Route("/signOut", name="sign_out", host="aesSidhe.fr")
+     */
+    public function signOut()
+    {
+		 return $this->redirectToRoute('landing_page');
+	}
+	
+	/**
+     * @Route("/register", name="register", host="aesSidhe.fr")
+     */
+    public function register(Request $request)
     {
 		$user = new User();
         $user->setUsername('MyUsername');
@@ -48,7 +75,7 @@ class ConnectionController extends Controller
 			return $this->redirectToRoute('landing_page');
 		}			
 			
-        return $this->render('connection/index.html.twig', [
+        return $this->render('security/register/index.html.twig', [
 			'form' => $form->createView(),
         ]);
     }
